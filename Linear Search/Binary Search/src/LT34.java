f1bba1e789d9d40755c5efeaef33a96ee6d77fdf
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2024-09-21 02:10:16 +0600

    Leetcode 34,744 added

--- Linear Search/Binary Search/src/LT34.java
+++ Linear Search/Binary Search/src/LT34.java
@@ -1,32 +1,50 @@
 //https://leetcode.com/problems/find-smallest-letter-greater-than-target/submissions/1396522309/
+import java.util.Arrays;
+
 public class LT34 {
     public static void main(String[] args) {
-        int[] nums = {5,7,7,8,8,10};
+        int[] nums = {5,7,7,7,8,8,10};
 
-        int[] result = searchRange(nums, 8);
-        System.out.println(result);
+        int[] result = searchRange(nums, 7);
+        System.out.println(Arrays.toString(result));
     }
 
     static int[] searchRange(int[] nums, int target) {
+        int[] result = {-1,-1};
+
+        int start = search(target, nums, true);
+        int end = search(target, nums, false);
+
+        result[0] = start;
+        result[1] = end;
+
+        return result;
+    }
+
+    static int search(int target, int[] nums, boolean findStartIndex){
+        int ans = -1;
+
         int start = 0;
         int end = nums.length - 1;
-        int[] result = {-1,-1};
+
         while(start <= end){
             int mid = start + (end - start)/2;
 
-            if(target == nums[mid]){
-                while(target == nums[mid]){
-                    mid--;
-                }
-                result[0] = mid + 1;
-                break;
-            }
-
-            else if(target >= nums[mid]) {
+            if(target > nums[mid]){
                 start = mid + 1;
             }
-            else end = mid - 1;
+            else if(target < nums[mid]){
+                end = mid - 1;
+            }
+            else {
+                ans = mid;
+                if(findStartIndex){
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
         }
-        return result;
+        return ans;
     }
 }
