be7b2d93e825e1cd8938df17969bf1c10e0be842
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2024-09-25 22:31:39 +0600

    Leetcode 33  added

--- Linear Search/Binary Search/src/LT33.java
+++ Linear Search/Binary Search/src/LT33.java
@@ -1,25 +1,30 @@
 //https://leetcode.com/problems/find-smallest-letter-greater-than-target/submissions/1396522309/
 public class LT33 {
     public static void main(String[] args) {
-        char[] letters = {'c', 'f', 'j'};
+        int[] nums = {4,5,6,7,0,1,2};
 
-        int result = nextGreatestLetter(letters, 'c');
-        System.out.println((result));
+        int pivot = findPivot(nums);
+        System.out.println(pivot);
     }
 
-    static char nextGreatestLetter(char[] letters, char target) {
+    static int findPivot(int[] nums){
         int start = 0;
-        int end = letters.length - 1;
+        int end = nums.length - 1;
 
         while(start <= end){
-            int mid = start + (end - start) / 2;
+            int mid = start + (end - start)/2;
 
-            if(target >= letters[mid]){
+            if(nums[mid] < nums[0] && nums[mid] < nums[mid - 1]){
+                return mid;
+            }
+            else if(nums[mid] > nums[0]){
                 start = mid + 1;
             }
-            else end = mid - 1;
+            else if(nums[mid] < nums[0] && nums[mid] > nums[mid - 1]){
+                end = mid - 1;
+            }
         }
 
-        return letters[start % letters.length];
+        return -1;
     }
 }
