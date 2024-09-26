30c43ce5893741d02a850baa17d93493d3b30f6a
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2024-09-26 21:17:01 +0600

    LT410 added

---
+++ Linear Search/Binary Search/src/LT410.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/find-smallest-letter-greater-than-target/submissions/1396522309/
+public class LT410 {
+    public static void main(String[] args) {
+        int[] nums = {5, 1, 2, 3, 4};
+
+
+    }
+
+    public int splitArray(int[]  nums, int m){
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i  <  nums.length)
+    }
+}
