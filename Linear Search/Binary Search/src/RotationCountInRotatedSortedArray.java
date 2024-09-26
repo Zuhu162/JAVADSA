7a7fec312ff50d9cd1f343633173a70f66fd715c
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2024-09-26 19:14:28 +0600

    Leetcode 33  added

--- Linear Search/Binary Search/src/RotationCountInRotatedSortedArray.java
+++ Linear Search/Binary Search/src/RotationCountInRotatedSortedArray.java
@@ -1,25 +1,31 @@
-//https://leetcode.com/problems/find-smallest-letter-greater-than-target/submissions/1396522309/
-public class LT744 {
+//https://www.geeksforgeeks.org/find-rotation-count-rotated-sorted-array/
+public class RotationCountInRotatedSortedArray {
     public static void main(String[] args) {
-        char[] letters = {'c', 'f', 'j'};
+        int[] arr1 = {15, 18, 2, 3, 6, 12};
+        int[] arr2 = {7, 9, 11, 12, 5};
+        int[] arr3 = {7, 9, 11, 12, 15};
 
-        int result = nextGreatestLetter(letters, 'c');
-        System.out.println((result));
+        System.out.println(findRotations(arr1)); // Output: 2
+        System.out.println(findRotations(arr2)); // Output: 4
+        System.out.println(findRotations(arr3)); // Output: 0
     }
 
-    static char nextGreatestLetter(char[] letters, char target) {
+    static int findRotations(int[] arr) {
         int start = 0;
-        int end = letters.length - 1;
+        int end = arr.length - 1;
 
         while(start <= end){
             int mid = start + (end - start) / 2;
 
-            if(target >= letters[mid]){
+
+            if(mid > 0 && arr[mid] < arr[mid - 1]){
+                return mid;
+            }
+            else if(arr[mid] > arr[arr.length - 1]){
                 start = mid + 1;
             }
             else end = mid - 1;
         }
-
-        return letters[start % letters.length];
+        return 0;
     }
 }
