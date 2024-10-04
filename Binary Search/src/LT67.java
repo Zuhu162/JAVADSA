9e52f6a1363cde6b240f60225136b0f74000f752
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2024-10-04 19:43:13 +0600

    LT41 added

---
+++ Binary Search/src/LT67.java
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/find-smallest-letter-greater-than-target/submissions/1396522309/
+public class LT67 {
+    public static void main(String[] args) {
+        long number = 2147483647;
+        int num = (int) number;
+
+        System.out.println(isPerfectSquare(num));
+    }
+
+    static boolean isPerfectSquare(int num) {
+        long start = 1;
+        long end = num;
+
+        while(start <= end){
+            long mid = start + (end - start) / 2;
+
+            if(mid * mid == num){
+                return true;
+            }
+            else if(mid * mid > num){
+                end = mid - 1;
+            }
+            else start = mid + 1;
+
+        }
+
+        return false;
+    }
+}
