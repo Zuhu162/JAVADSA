325aa013159d32b110da59a2eb122aabeddea1c1
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2024-10-10 23:07:26 +0600

    Recursion

---
+++ Recursion/src/Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int ans = findFactorial(5);
+        System.out.println(ans);
+    }
+
+    static int findFactorial(int n){
+            if(n == 1){
+                return 1;
+            }
+
+            return n * findFactorial(n-1);
+    }
+}
