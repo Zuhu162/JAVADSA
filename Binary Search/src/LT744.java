059d3822a700123b3d8f06e2f31b0d6fa36e3a08
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2025-03-04 23:30:02 +0600

    Fixed LT744 error

--- Binary Search/src/LT744.java
+++ Binary Search/src/LT744.java
@@ -1,21 +1,24 @@
 //https://leetcode.com/problems/find-smallest-letter-greater-than-target/submissions/1396522309/
 public class LT744 {
-    void main(){
+    public static void main(String[] args){
         char[] letters = {'c', 'f', 'j'};
-        System.out.println(nextGreatestLetter(letters, 'a'));
+        char result = nextGreatestLetter(letters, 'c');
+        System.out.println(result);
     }
 
-    char nextGreatestLetter(char[] letters, char target){
+    static char nextGreatestLetter(char[] letters, char target){
         int start = 0;
         int end = letters.length - 1;
 
         while(start <= end){
             int mid = start + (end - start) / 2;
+
             if(target >= letters[mid]){
                 start = mid + 1;
             }
             else end = mid - 1;
         }
+
         return letters[start % letters.length];
     }
 }
